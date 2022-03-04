807d84a18b704a1589bf9b551a92f96acd5f1a6c
karopr <karopr8@gmail.com>
2022-03-04 10:39:43 -0500

    cambios en js

--- js/acierto-error.js
+++ js/acierto-error.js
@@ -2,55 +2,7 @@ function acierto(lasTarjetas) {
   lasTarjetas.forEach(function(elemento) {
     elemento.classList.add("acertada");
   });
-  //document.querySelector("#sonido-acierto").play();
-
-  if(lasTarjetas[1].dataset.valor==="alpaca.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="amazonia.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="bandera.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="carnaval.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="castillo.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="chanchan.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="costa.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="festejo.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="gallito.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="huascaran.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="lima.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="machuPicchu.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="marinera.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="montana.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="plaza.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
-
-  if(lasTarjetas[1].dataset.valor==="selva.png"){
-    document.querySelector("#sonido-error").cloneNode().play()}
+  document.querySelector("#sonido-acierto").play();
   }
  //
 
